cf136da9d2c49024d66edff0aa692b5da72efabb
Albert <albert@cuadrantedigital.com>
2024-02-22 17:11:19 -0500

    Add php Model

---
+++ src/Toggl.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CuadranteDigital\TogglModel;
+
+class Toggl
+{
+    private $container;
+    private $settings;
+    
+    public function __construct()
+    {
+        
+    }    
+}
